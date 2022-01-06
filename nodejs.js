cae63ee03ec19e6106fb70588244fcd5b79b1ee2
Anita Cheung <acheung@grailbio.com>
2022-01-05 21:16:08 -0800

    added description to information function

--- nodejs.js
+++ nodejs.js
@@ -1,4 +1,13 @@
 /*
 References:
 https: //www.freecodecamp.org/news/rest-api-tutorial-rest-client-rest-service-and-api-calls-explained-with-code-examples/
+https://www.w3schools.com/nodejs/
 */
+
+function information() {
+    var description = "Representational state transfer (REST) - software architectural style that defines a set of constraints to be used for creating Web services
+    "1. Client-Server architecture (client and server should be separate),"
+    "2. Statelessness (each request should be made with all required data),"
+    "3. Layered system (should not distinguish between proxies and load balancers)";
+    console.log(descripion);
+}
